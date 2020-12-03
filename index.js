d1a85bbfb7d2d9169958293b36da8bc6ef7e783d
Marek Matczak <mmatczak@autograf.pl>
2020-12-03 10:43:22 +0100

    playing with JavaScript objects

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Hello World');
+const person = {
+    name: 'Marek',
+    age: 41,
+    male: true,
+    birthDate: '1979-01-25'
+};
+
+
+class Person {
+    constructor(name) {
+        this.name = name || '';
+        this.age = 41
+    }
+}
+
+const propertyName = 'name'
+const marek = new Person('Marek');
+// console.log(marek.name);
+// console.log(marek[propertyName]);
+marek.address = {
+    zipCode: '53-601'
+}
+
+let zipCode = (marek && marek.address && marek.address.zipCode) || '';
+console.log(zipCode);
+
+delete marek.address
+zipCode = (marek && marek.address && marek.address.zipCode) || '';
+console.log(zipCode);
+
+// Object.keys(marek).forEach(
+//     propertyName => console.log(`${propertyName} -> ${marek[propertyName]}`));
+
