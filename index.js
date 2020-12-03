d9e3d39753171c734cc257ad362a676cf5e9fafd
Marek Matczak <mmatczak@autograf.pl>
2020-12-03 10:54:14 +0100

    playinf

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-const person = {
-    name: 'Marek',
-    age: 41,
-    male: true,
-    birthDate: '1979-01-25'
-};
+// function expression
+// const add = function (a, b) {
+//     return a + b;
+// };
+
+// arrow function (aka lambda)
+const add = (a, b) => a + b;
+
+console.log(add(4, 3));
 
 
-class Person {
-    constructor(name) {
-        this.name = name || '';
-        this.age = 41
-    }
-}
-
-const propertyName = 'name'
-const marek = new Person('Marek');
-// console.log(marek.name);
-// console.log(marek[propertyName]);
-marek.address = {
-    zipCode: '53-601'
-}
-
-let zipCode = (marek && marek.address && marek.address.zipCode) || '';
-console.log(zipCode);
-
-delete marek.address
-zipCode = (marek && marek.address && marek.address.zipCode) || '';
-console.log(zipCode);
-
-// Object.keys(marek).forEach(
-//     propertyName => console.log(`${propertyName} -> ${marek[propertyName]}`));
-
+// function declaration
+// function add(a, b) {
+//     return a + b;
+// }
