5bbf2a9047ac607d35961bfb9394664f4bdad264
Marek Matczak <mmatczak@autograf.pl>
2020-12-04 15:37:24 +0100

    UserCtrl.onSave tested

--- user-controller.test.js
+++ user-controller.test.js
@@ -1,39 +1,74 @@
 const UserCtrl = require('./user-controller')
-const UserService = require('./user-service')
 
 describe('UserCtrl', function () {
-    it('initializes form with values from service', () => {
-        // given
-        expect.hasAssertions();
-        const firstName = 'Test First';
-        const lastName = 'Test Last';
+    describe('DOM', () => {
+        let userId, firstName, lastName, userServiceMock, formHtml, controller, updatePromise;
 
-        const userServiceMock = {
-            getOne: jest.fn(id => Promise.resolve({id, firstName, lastName}))
-        }
-        const userId = 1234;
-        const controller = new UserCtrl(userServiceMock, userId);
-        document.body.innerHTML = `
+        beforeEach(() => {
+            userId = 1234;
+            firstName = 'Test First';
+            lastName = 'Test Last';
+
+            userServiceMock = {
+                getOne: jest.fn(id => Promise.resolve({id, firstName, lastName})),
+                update: jest.fn(user => updatePromise = Promise.resolve(user))
+            }
+
+            controller = new UserCtrl(userServiceMock, userId);
+
+            formHtml = `
         <form>
         <label for="firstName">First name:</label>
         <input id="firstName">
         <label for="lastName">Last name:</label>
         <input id="lastName">
         <button>Save</button>
-        </form>`;
-        // when
-        return controller.initializeForm()
-            .then(() => {
-                // then
-                expect(userServiceMock.getOne).toHaveBeenCalledWith(userId);
+        </form>`
+        });
 
+        it('initializes form with values from service', () => {
+            // given
+            expect.hasAssertions();
+            document.body.innerHTML = formHtml;
+            // when
+            return controller.initializeForm()
+                .then(() => {
+                    // then
+                    expect(userServiceMock.getOne).toHaveBeenCalledWith(userId);
+
+                    const firstNameInput = document.querySelector('#firstName');
+                    expect(firstNameInput).not.toBeNull();
+                    expect(firstNameInput.value).toBe(firstName);
+
+                    const lastNameInput = document.querySelector('#lastName');
+                    expect(lastNameInput).not.toBeNull();
+                    expect(lastNameInput.value).toBe(lastName);
+                });
+        });
+
+        it('updates user data on save', () => {
+            // given
+            const updatedFirstName = 'Updated First'
+            const updatedLastName = 'Updated Last'
+            expect.hasAssertions();
+            document.body.innerHTML = formHtml;
+            return controller.initializeForm().then(() => {
+                // when
                 const firstNameInput = document.querySelector('#firstName');
-                expect(firstNameInput).not.toBeNull();
-                expect(firstNameInput.value).toBe(firstName);
-
+                firstNameInput.value = updatedFirstName
                 const lastNameInput = document.querySelector('#lastName');
-                expect(lastNameInput).not.toBeNull();
-                expect(lastNameInput.value).toBe(lastName);
+                lastNameInput.value = updatedLastName
+                const buttonElement = document.querySelector('button');
+                buttonElement.click();
+                // const form = document.querySelector('form');
+                // form.dispatchEvent(new Event('submit'));
+                // then
+                return updatePromise.then(() => {
+                    expect(userServiceMock.update).toHaveBeenCalledWith({
+                        id: userId, firstName: updatedFirstName, lastName: updatedLastName
+                    });
+                });
             });
+        });
     });
 });
