1248ba56b3fb82ce45122054279d532038e22912
Marek Matczak <mmatczak@autograf.pl>
2020-12-04 08:39:47 +0100

    exercise commented

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 console.log('Hello world');
+const userId = 1234;
+// create UserCtrl
+// initialize the form with user data got from the UserService
+// on save button call the update method of the UserService
+
+
+// create UserService having two methods:
+// getOne() returning Promise of User data (1 s delay)
+// update() returning Promise and logging the updated user to the console (1 s delay)
 
